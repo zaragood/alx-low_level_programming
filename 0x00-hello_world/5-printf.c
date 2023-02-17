3716ecdc2342e2669672df577da424e029c9c16c
zaragood <azaragoodness52@gmail.com>
2023-02-16 16:45:27 -0800

    A program that uses the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses printf function to print text to standard output
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
