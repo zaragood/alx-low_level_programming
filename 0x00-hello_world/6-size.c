b734ad18c1fb56dc047f3aecdcb1ef269835ca90
zaragood <azaragoodness52@gmail.com>
2023-02-17 03:12:14 -0800

    sizeof operation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * sizeof - size of evaluates the size of a variable
+ * main - size of evaluates the size of a variable
  * Return: 0
  */
 int main(void)
@@ -8,14 +8,14 @@ int main(void)
 	int intType;
 	float floatType;
 	long longType;
-	double doubleType;
+	long long int doubleType;
 	char charType;
 
+	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
 	printf("Size of a int: %lu bytes\n", sizeof(intType));
 	printf("Size of a float: %lu bytes\n", sizeof(floatType));
 	printf("Size of a long: %lu bytes\n", sizeof(longType));
-	printf("Size of a double: %lu bytes\n", sizeof(doubleType));
-	printf("Size of a char: %lu byte\n", sizeof(charType));
+	printf("Size of a long long int: %lu bytes\n", sizeof(doubleType));
 	return (0);
 }
 
