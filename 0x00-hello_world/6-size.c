1eb52048b012f3015dd3e753d445998520fc4fc6
zaragood <azaragoodness52@gmail.com>
2023-02-17 03:17:45 -0800

    size of operation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	int intType;
 	float floatType;
-	long longType;
+	long int longType;
 	long long int doubleType;
 	char charType;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %lu bytes\n", sizeof(intType));
-	printf("Size of a float: %lu bytes\n", sizeof(floatType));
-	printf("Size of a long: %lu bytes\n", sizeof(longType));
-	printf("Size of a long long int: %lu bytes\n", sizeof(doubleType));
+	printf("Size of a int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(doubleType));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 	return (0);
 }
 
