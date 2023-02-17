400c1eb4cdddca6b65ca9133ceb3a2750fb7293c
zaragood <azaragoodness52@gmail.com>
2023-02-16 16:23:27 -0800

    program that prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-#!/bin/bash
-#iclude <stdio.h>
-
+#include <stdio.h>
+/**
+ * A program that prints out a text to the console
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
