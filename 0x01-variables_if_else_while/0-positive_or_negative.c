1d568b02117c766ddf640efaa865e2358730dee6
zaragood <azaragoodness52@gmail.com>
2023-02-17 14:16:59 -0800

    if else statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * if - to specify a block of code to be executed
+ * else if - to specify a new condition
+ * else - Execute if the same condition is false
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+if n > 0
+{
+	printf("%d\n is positive", n);
+}
+else if (n < 0)
+{
+	printf("%d\n is negetive", n);
+}
+else
+{
+	printf("%d\n is zero", n);
+}
+	return (0);
+}
