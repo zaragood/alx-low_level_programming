bc4db7cadd7c9d9e1b334d9e0e62521413e74b07
zaragood <azaragoodness52@gmail.com>
2023-02-17 04:48:00 -0800

    A program that uses system function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses fprintf to print standard error
+ * Return: Always 1
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
