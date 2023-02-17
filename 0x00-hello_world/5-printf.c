7b55921bb07a9d051d1ba8a8ca2892245eefbea2
zaragood <azaragoodness52@gmail.com>
2023-02-17 03:01:27 -0800

    printf()function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that uses printf function to print test tot console
- * Retun: 0
+ * main - Entry point
+ * A program that uses a printf function to print text to console
+ * Return: Always 0
  */
 int main(void)
 {
