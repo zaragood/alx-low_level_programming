0420d34a20883049bc049694f4813aa484576a58
zaragood <azaragoodness52@gmail.com>
2023-02-16 16:31:27 -0800

    Programming is like building a multilingual puzzel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * A program that prints out a text to the console
- */
+ *main - A program that prints out a text to the console
+ *Return: return an integer
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
