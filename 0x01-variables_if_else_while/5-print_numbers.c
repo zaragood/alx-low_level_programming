3a6a22e1e14f5717580fd16ac0b745dc2b629234
zaragood <azaragoodness52@gmail.com>
2023-02-18 15:46:18 -0800

    prints numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * printf - to print a string
+ * Return: always 0
+ */
+int main(void)
+{
+	int x;
+
+	for  (x = 0; x < 10; x++)
+	{
+	printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
