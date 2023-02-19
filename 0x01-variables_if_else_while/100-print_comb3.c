985a46b9b1f1d7d611b06b5c6c2ba3489b2526d5
zaragood <azaragoodness52@gmail.com>
2023-02-19 13:20:09 -0800

    combination of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,24 @@
 int main(void)
 {
 	int num1, num2;
+	int num3 = 0;
 
-	for (num1 = 0; num1 < 10; num1++)
+	if (num3 < 10)
 	{
-		for (num2 = 0; num2 < 10; num2++)
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num3; num2 < 10; num2++)
 		{
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
+			if (num1 != num2)
+			{
+			putchar((num1) + '0');
+			putchar((num2) + '0');
 
 			putchar(',');
 			putchar(' ');
+			}
+		}
+		num3++;
 		}
 	}
 	putchar('\n');
