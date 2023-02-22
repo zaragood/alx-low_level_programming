e33087d3a4c5a5d19f8b2348c6a0babe566a41ca
zaragood <azaragoodness52@gmail.com>
2023-02-22 01:34:14 -0800

    checks for lower case letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - a function that checks for lower case alphabet
+ *
+ * Return: always (0)
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
