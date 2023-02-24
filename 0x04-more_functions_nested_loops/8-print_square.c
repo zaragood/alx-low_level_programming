b3b3f5deb3035532b1922ba4b0b35bb8eb422d59
zaragood <azaragoodness52@gmail.com>
2023-02-24 05:03:57 -0800

    prints squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,4 +14,15 @@ void print_square(int size)
 	{
 		_putchar('\n');
 	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
