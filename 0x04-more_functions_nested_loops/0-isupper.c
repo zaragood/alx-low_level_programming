25f452b94f4039989eccc637769c8602c42256c4
zaragood <azaragoodness52@gmail.com>
2023-02-23 16:06:52 -0800

    isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - upper case alphabet
+ * @c: a character
+ *
+ * Return: always 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
