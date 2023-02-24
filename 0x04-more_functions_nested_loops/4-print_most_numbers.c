640bcb5d63f21726913733643e90114d4be6a222
zaragood <azaragoodness52@gmail.com>
2023-02-23 17:17:02 -0800

    except 2 and four

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints most numbers
+ * void: returns nothing
+ *
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+	{
+		_putchar((i) + '0');
+
+		if (i != 2 && i != 4)
+		{
+			_putchar((i) + '0');
+		}
+
+	}
+	 
+}
