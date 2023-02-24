53e4f68a138b5ed6a27b9b9209c9c7916cdddd06
zaragood <azaragoodness52@gmail.com>
2023-02-23 19:34:51 -0800

    digonal line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_line - program that prints line
+ * @n: number of lines
+ *
+ * Reaturn: always 0
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+	_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
