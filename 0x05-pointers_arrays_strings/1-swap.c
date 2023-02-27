2069aca61c88ac960b74d3522a983b73c2d6a625
zaragood <azaragoodness52@gmail.com>
2023-02-27 05:30:38 -0800

    swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - function that swap the value of two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
+
