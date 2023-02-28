5ad4b90a8f046d3016d320d3adcda1ed399fa858
zaragood <azaragoodness52@gmail.com>
2023-02-28 12:11:35 -0800

    skips the second number in a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#
+/**
+ * _puts - takes in string
+ * @str: string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,10 @@ void puts2(char *str)
 {
 	int i;
 
-	i = 0;
-
-	for (i = 0; str[i] != '\n'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] <= 4)
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
