18caea99a1b092b2dd68665b8bdce30cce2f55eb
zaragood <azaragoodness52@gmail.com>
2023-03-01 14:02:28 -0800

    prints string in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,14 +4,22 @@
  * @s: string
  * Return: 0
  */
-void rev_string(char *s);
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
+ */
+void rev_string(char *s)
 {
-	int a, b;
+	int len = 0, index = 0;
+	char tmp;
 
-	b = 0;
+	while (s[index++])
+		len++;
 
-	while (s[b] != '\0')
-		b++;
-
-		for (a = b  - 1; a >= 0; a--)
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
+	}
 }
