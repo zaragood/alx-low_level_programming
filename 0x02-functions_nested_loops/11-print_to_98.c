290c326dff6ce28998c0b01574f04423ef9e0660
zaragood <azaragoodness52@gmail.com>
2023-03-04 13:40:03 -0800

    all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,9 +11,9 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while(n < 98)
+		while (n < 98)
 		{
-			printf("%d ,", n++);
+			printf("%d, ", n++);
 		}
 		printf("%d\n", n);
 	}
@@ -21,7 +21,7 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d ,", n--);
+			printf("%d, ", n--);
 		}
 		printf("%d\n", n);
 	}
