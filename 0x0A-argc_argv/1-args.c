a42acb7c84f745cf229955a34e66c05e17b58b08
zaragood <azaragoodness52@gmail.com>
2023-03-10 20:25:06 -0800

    program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: arguement count
+ * @argv: argument command line
+ *
+ * Return: always 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+0X0A-argc_argv
+ALX software engineering project on argument and mains
+by Azara Chibueyim
