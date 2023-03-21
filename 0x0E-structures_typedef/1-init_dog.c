4e1695de59739c8af9e834f7feb2d160b1429246
zaragood <azaragoodness52@gmail.com>
2023-03-21 14:46:03 -0700

    memory was is allocated to the pointer d

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - a C function that takes a pointer to a struct dog
  * @d: variable that should be initialized
@@ -8,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
