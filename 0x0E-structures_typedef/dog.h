e1808ab79af19b298436417b11f98d2819db6b83
zaragood <azaragoodness52@gmail.com>
2023-03-21 12:41:25 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines a new type of struct
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ * Description: The `struct dog` is a C structure that represents a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
