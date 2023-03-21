1ba5315b821c249dc60f64d0b00cd0d86132cafc
zaragood <azaragoodness52@gmail.com>
2023-03-21 15:41:05 -0700

    updated function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,19 +7,21 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	if (d->name == NULL)
-		printf("nil");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("nil");
-	else
-		printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("nil");
-	else
-		printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("nil");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age == 0)
+			printf("nil");
+		else
+			printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("nil");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+	return;
 }
 
