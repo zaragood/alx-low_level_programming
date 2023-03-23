5d66e34b0e8c9c343a6070f7b2b20b48ead416bb
zaragood <azaragoodness52@gmail.com>
2023-03-23 16:39:14 -0700

    updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*(argv[2]) == '/') || (*(argv[2]) == '%') && num2 == 0)
+	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
