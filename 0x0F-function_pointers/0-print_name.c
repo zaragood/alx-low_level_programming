31ef0a98c506b2a2b4b56c792d7fa10a52449f2e
zaragood <azaragoodness52@gmail.com>
2023-03-23 01:38:21 -0700

    pointer to function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
  * print_name - A function name of type void
  * @name: a pointer to a character array that contains the name to be printed
- * @f: a pointer to a function that takes a pointer to a character array as an argument and returns void
+ * @f: a pointer to a function that takes a pointer to a character array
  */
 void print_name(char *name, void (*f)(char *))
 {
