4144a7888210a2f5f3bc33617291e97501267714
zaragood <azaragoodness52@gmail.com>
2023-03-23 17:03:48 -0700

    updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,14 +13,13 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	if (argv[2][1] || !get_op_func(argv[2]))
 	{
