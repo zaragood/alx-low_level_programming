7899af35be363fb22a290969243a2961721ccc73
zaragood <azaragoodness52@gmail.com>
2023-04-04 05:10:37 -0700

    file added

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - function that prints all elements of a list
+ * @h: pointer to first node
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num_nodes++;
+		h = h->next;
+	}
+	return (num_nodes);
+}
