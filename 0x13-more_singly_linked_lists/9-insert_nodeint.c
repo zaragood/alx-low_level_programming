920168cf5f08a38a021569561dad94cfbf2213a4
zaragood <azaragoodness52@gmail.com>
2023-04-05 14:42:38 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
 	while (i < idx && temp != NULL)
 	{
 		temp = temp->next;
