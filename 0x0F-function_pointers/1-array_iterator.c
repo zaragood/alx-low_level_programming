22fbeca9d9fcc6dbdf6551fa5694017aa06a3cc3
zaragood <azaragoodness52@gmail.com>
2023-04-20 12:24:46 -0700

    function that prints an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function to an array
+ * @array: elements in an array
+ * @size: size of the array
+ * @action: pointer to the function to be used
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
