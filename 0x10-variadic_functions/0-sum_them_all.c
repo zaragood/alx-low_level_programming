4d7d186cfd1096bff99370943537c8f093b235f1
zaragood <azaragoodness52@gmail.com>
2023-04-20 05:11:53 -0700

    betty stlying

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,17 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, number, sum = 0;
 	va_list list;
 
-	 if (n == 0)
-		 return (0);
+	if (n == 0)
+		return (0);
 
-	 va_start(list, n);
-	 for (i = 0; i < n; i++)
-	 {
-		 number = va_arg(list, unsigned int);
-		 sum += number;
-	 }
-	 va_end(list);
-	 return (sum);
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		number = va_arg(list, unsigned int);
+		sum += number;
+	}
+	va_end(list);
+	return (sum);
 
 
 }
