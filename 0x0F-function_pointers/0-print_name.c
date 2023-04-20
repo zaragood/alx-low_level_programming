fa111a9c3c16021611fcb885862651ffc4faff66
zaragood <azaragoodness52@gmail.com>
2023-04-20 08:37:49 -0700

    function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: function pointer to print_name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(f)(name);
+}
