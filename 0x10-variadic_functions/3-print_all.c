f06196c36e0524e00f5bc7184f1fb9e71789affd
zaragood <azaragoodness52@gmail.com>
2023-04-21 06:58:27 -0700

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,77 +1,45 @@
 #include "variadic_functions.h"
 /**
- * print_char - prints a character
- * @arg: argument to be printed
+ * print_all - function that prints anything
+ * @format: list of arguments
  */
-void print_char(va_list arg)
-{
-	printf("%c", va_arg(arg, int));
-}
 
-/**
- * print_int - prints an integer
- * @arg: argument to be printed
- */
-void print_int(va_list arg)
-{
-	printf("%i", va_arg(arg, int));
-}
-
-/**
- * print_float - prints a float number
- * @arg: argument to be printed
- */
-void print_float(va_list arg)
-{
-	printf("%f", va_arg(arg, double));
-}
-
-/**
- * print_str - prints a string
- * @arg: argument to be printed
- */
-void print_string(va_list arg)
-{
-	char *string = va_arg(arg, char *);
-
-	if (string == NULL)
-		string = "(nil)";
-	printf("%s", string);
-}
-
-/**
- * print_all- prints anything
- * @format: list of arguments passed
- */
 void print_all(const char * const format, ...)
 {
-	va_list list;
-	int i = 0, k = 0;
-	char *character = "";
+	va_list lists;
+	char *string;
+	int i = 0;
 
-	opt format_funcs[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-	};
+	va_start(lists, format);
 
-	va_start(list, format);
 	while (format && format[i])
 	{
-		while (k < 4)
+		switch (format[i])
 		{
-			if (format[i] == format_funcs[k].symbol)
-			{
-				printf("%s", character);
-				format_funcs[k].ptr(list);
-				character = ", ";
+			case 'c':
+				printf("%c", va_arg(lists, int));
 				break;
-			}
-			k++;
+			case 'i':
+				printf("%d", va_arg(lists, int));
+				break;
+			case 'f':
+				printf("%f", (float) va_arg(lists, double));
+				break;
+			case 's':
+				string = va_arg(lists, char *);
+				if (string == NULL)
+					printf("(nil)");
+				else
+					printf("%s", string);
+				break;
+			default:
+				i++;
+				continue;
 		}
+		if (format[i + 1] != '\0')
+			printf(", ");
 		i++;
 	}
 	printf("\n");
-	va_end(list);
+	va_end(lists);
 }
