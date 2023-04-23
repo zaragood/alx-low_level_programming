0066454e06c8521cb8b10237df05680e4ecf7b70
zaragood <azaragoodness52@gmail.com>
2023-04-23 13:50:20 -0700

    program thaqt prints minimum number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,49 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * cal_num_coins - function to calculate the minimum nuber of coins
- * @cents: the amount of cents you need to give back
- * @coin_value: unlimited number of coins of value
- *
- * Return: error if argument is not 1 or 0 if number is negetive
- */
-int cal_num_coins(int cents, int coin_value)
-{
-	int num_coins = cents / coin_value;
-
-	cents %= coin_value;
-	return (num_coins);
-}
-/**
  * main - the main function of the program
  * @argc: argument count
  * @argv: argument value
  *
  * Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int num_coins = 0, num, j, i = 0;
-	char *value;
+	int num_coins = 0, value, i = 0, num;
+	int number[] = {25, 10, 5, 2, 1};
 
-	if (argc == 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
-		return (0);
+		return (1);
 	}
-	for (i = 1; i < argc; i++)
+	else
 	{
-		value = argv[i];
-		for (j = 0; value[j]; j++)
+		value = atoi(argv[1]);
+
+		if (value  < 0)
 		{
-			if (value[j] < '0' || value[j] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("0\n");
+			return (0);
 		}
-		num += atoi(value);
-		num_coins +=  num;
+
+		num = sizeof(number) / sizeof(number[0]);
+		for (i = 0; i < num; i++)
+		{
+			num_coins += value / number[i];
+			value %= number[i];
+		}
 	}
 	printf("%d\n", num_coins);
 	return (0);
