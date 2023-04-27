332979beb72fb572ffb0e7e33a959f59af1e5134
zaragood <azaragoodness52@gmail.com>
2023-04-27 13:59:34 -0700

    assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,26 @@
+extern printf
+
+section	.data
+	msg	db	'Hello, Holberton', 0
+	fmt	db	'%s\n', 0
+
+section	.text
+	global	main
+
+main:
+	; Set up stack frame
+	push	rbp
+	mov	rbp, rsp
+
+	; Call printf with message and format string
+	mov	rdi, fmt
+	mov	rsi, msg
+	xor	rax, rax
+	call	printf
+
+	; Clean up stack and return
+	mov	rsp, rbp
+	pop	rbp
+	xor	eax, eax
+	ret
+
