ba168008d48cae669e2c3c486af8c6d7f3dae93f
zaragood <azaragoodness52@gmail.com>
2023-04-27 13:07:10 -0700

    betty style correction

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,13 +10,14 @@
  */
 void free_list(list_t *head)
 {
-    list_t *temp;
+	list_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp->str);
-	free(temp);
-    }
+	/* traverse the list*/
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
