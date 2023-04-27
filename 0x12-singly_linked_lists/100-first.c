94ac5f98874fd328366d76b0421f4bcdebdf4c7e
zaragood <azaragoodness52@gmail.com>
2023-04-27 13:30:34 -0700

    befor main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * before_main - Function that prints a message before main function is executed
+ *
+ * Description: This function is decorated with the `__attribute__ ((constructor))`
+ * function attribute, which causes it to be executed automatically before the
+ * `main` function is executed. The function simply prints the message "You're beat!
+ * and yet, you must allow,\nI bore my house upon my back!\n" to the console using
+ * the `printf` function.
+ *
+ * Return: void
+ */
+
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
