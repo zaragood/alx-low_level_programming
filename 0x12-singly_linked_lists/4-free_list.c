7c848517522c154d191b52927bf433508ace6d46
zaragood <azaragoodness52@gmail.com>
2023-04-27 13:02:53 -0700

    frees node

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_list - Frees a linked list_t list
+ * @head: Pointer to the head of the linked list
+ *
+ * Description: This function frees all the nodes of a linked list_t list.
+ * It starts at the head of the list and iterates through all the nodes,
+ * freeing the memory allocated for each node. The function does not return
+ * anything.
+ */
+void free_list(list_t *head)
+{
+    list_t *temp;
+
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->str);
+	free(temp);
+    }
+}
