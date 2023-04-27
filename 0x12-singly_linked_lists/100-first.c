562be4702b0815031cb0f9a3f7f719047acce5b8
zaragood <azaragoodness52@gmail.com>
2023-04-27 13:38:43 -0700

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * before_main - Function that prints a message before main function is executed
- *
- * Description: This function is decorated with the `__attribute__ ((constructor))`
+ * Description: This function is decorated with the __attribute__constructor
  * function attribute, which causes it to be executed automatically before the
- * `main` function is executed. The function simply prints the message "You're beat!
- * and yet, you must allow,\nI bore my house upon my back!\n" to the console using
- * the `printf` function.
+ * `main` function is executed. The function simply prints the message
+ * "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+ * to the console using the `printf` function.
  *
  * Return: void
  */
