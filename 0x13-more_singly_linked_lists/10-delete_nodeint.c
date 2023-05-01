f4362ede00457c8e92e8eeb5adfe0b1fa6617991
zaragood <azaragoodness52@gmail.com>
2023-05-01 14:16:39 -0700

    updated

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	/* Check if list is empty */
 	if (*head == NULL)
 		return (-1);
-	
+
 	temp = *head;
 
 	/* Check if head node needs to be deleted */
