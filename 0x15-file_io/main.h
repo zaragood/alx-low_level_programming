b2f8f539eed32904c3b58beedaf970714513172b
zaragood <azaragoodness52@gmail.com>
2023-05-10 04:50:42 -0700

    task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unstid.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
