e8cfd9e8dc6e19da7643a52dad4b3340c084100f
zaragood <azaragoodness52@gmail.com>
2023-08-21 07:58:17 -0700

    script file for creating dynamic library

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Compile all .c files into object files
+gcc -c -fPIC *.c
+
+# Create the dynamic library
+gcc -shared -o liball.so *.o
+
+# Clean up by removing the object files
+rm *.o
+
+echo "Dynamic library liball.so created."
+
