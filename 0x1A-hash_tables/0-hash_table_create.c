965b37b6cc91506cecc6bc87079c8f7edde0122c
zaragood <azaragoodness52@gmail.com>
2023-09-08 22:17:00 +0000

    betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,26 +12,26 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = NULL;
 
 	/* Allocates memory for the hash table*/
-	table = (hash_table_t*) malloc(sizeof(hash_table_t));
-	
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+
 	/*check if memory ewas sucessully allocated*/
 	if (table == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
 	/*check if memory ewas sucessully allocated*/
-        if (table->array == NULL)
-        {
-                return (NULL);
-        }
+	if (table->array == NULL)
+	{
+		return (NULL);
+	}
 
-        for (i = 0; i < table->size; i++)
-        {
-                table->array[i] = NULL;
-        }
-	
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+
 	return (table);
 }
