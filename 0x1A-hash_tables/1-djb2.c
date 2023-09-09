aa26736e251cb6486832f6c449b7b4d6937ccd08
zaragood <azaragoodness52@gmail.com>
2023-09-09 15:14:24 +0000

    function implementing the djb2 algorithm.

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm.
+ * @str: data to be hashed
+ * Return: the hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash_value;
+	int c;
+
+	hash_value = 5381;
+	while((c = *str))
+	{
+		hash_value = ((hash_value << 5) + hash_value) + c; /* hash * 33 + c */
+		str++;
+	}
+	return (hash_value);
+}
