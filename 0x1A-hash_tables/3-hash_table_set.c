15ad70bddbacdbbf8b6b6568960237821c1565a7
zaragood <azaragoodness52@gmail.com>
2023-09-12 10:05:25 +0000

    updated code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,7 +5,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	char *value_copy = NULL;
+	char *value_copy = NULL, *key_copy = NULL;
 	hash_node_t *new_node= NULL;
 
 	if (ht == NULL && key == NULL)
@@ -13,34 +13,38 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	/* duplicate the string*/
+	key_copy = strdup(key);
+	if (key_copy == NULL)
+        {
+                return (0);
+        }
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 	{
+		free(key_copy);
 		return (0);
 	}
 	/*get the index of the key*/
 	index = key_index((const unsigned char *)key, ht->size);
 
+	/*if the key exits*/
 	if (ht->array[index] && *(ht->array[index]->key) == *key)
 	{
 		free(ht->array[index]->value);
 		ht->array[index]->value = value_copy;
 		return (1);
 	}
+	/*if the key does not exits then create a memory for the new key and value*/
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		free(value_copy);
+		free(key_copy);
 		return (0);
 	}
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(value_copy);
-		free(new_node->key);
-		return (0);
-	}
-	new_node-> value = (value_copy);
+	/*initialize the node*/
+	new_node->key = key_copy;
+	new_node->value = value_copy;
 	new_node->next = NULL;
 
 	if (ht->array[index])
