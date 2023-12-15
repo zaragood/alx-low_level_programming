0db96ae8d0bab72d3aee2f1780d9bb89f84528f2
Official_Rhema_Daniel <rhemadaniel2004@gmail.com>
2023-12-15 23:01:13 +0000

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t low, size_t high);
+int binary_helper(int *array, size_t low, size_t high, int value);
 #endif /*SEARCH_ALGOS_H*/
